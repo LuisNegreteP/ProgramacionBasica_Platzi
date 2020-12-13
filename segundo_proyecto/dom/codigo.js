60eb116f2099dcb93eb1475c5c4be369b818f6b9
Luis Negrete <cachis432@gmail.com>
2020-12-13 16:24:14 -0600

    Prueba de DOM

---
+++ segundo_proyecto/dom/codigo.js
@@ -0,0 +1,3 @@
+var ruta = window.location;        //Guarda la dirección de la ventana en la variable ruta
+console.log (ruta);               //Mensaje en consola para ver sus elementos
+document.write("Estás en: "+ruta); //Escribe la ruta en el html
